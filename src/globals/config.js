d1db454d442a498d9846c17dd0ed6f34ce8d60b3
Putri Benedicta <pbenedictaa@gmail.com>
2021-12-24 18:32:05 +0700

    delete unused files

--- src/globals/config.js
+++
@@ -1,8 +0,0 @@
-// import materi from "../materi-ajar.json";
-const CONFIG = {
-    DATABASE_NAME: 'careit-database',
-    DATABASE_VERSION: 1,
-    OBJECT_STORE_NAME: 'care-it',
-};
-
-export default CONFIG;
